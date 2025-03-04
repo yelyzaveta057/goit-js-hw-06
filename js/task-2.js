3c628a5ff4a0e4d2fbea407edfbe29c2118f8630
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-03-03 21:32:30 -0500

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,31 @@
-function makeArray(firstArray, secondArray, maxLength){
-    const newArray = firstArray.concat(secondArray);
-    return newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray;
-}
-
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+class Storage {
+    constructor(items) {
+      this.items = items;
+    }
+    
+    getItems() {
+      return this.items;
+    }
+    
+    addItem(newItem) {
+      this.items.push(newItem);
+    }
+    
+    removeItem(itemToRemove) {
+      this.items = this.items.filter(item => item !== itemToRemove);
+    }
+  }
+  
+  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+  
+  storage.addItem("Droid");
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+  
+  storage.removeItem("Prolonger");
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+  
+  storage.removeItem("Scaner");
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+  
+  
