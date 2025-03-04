dc97348365f8aa6d0ee3f7a00498c1f83addf6cc
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-03-04 12:41:54 -0500

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,26 @@
 class StringBuilder {
-    constructor(initialValue) {
-      this.value = initialValue;
-    }
-    
-    getValue() {
-      return this.value;
-    }
-    
-    padEnd(str) {
-      this.value += str;
-    }
-    
-    padStart(str) {
-      this.value = str + this.value;
-    }
-    
-    padBoth(str) {
-      this.value = str + this.value + str;
-    }
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
   }
+  
+  getValue() {
+    return this.#value;
+  }
+  
+  padEnd(str) {
+    this.#value += str;
+  }
+  
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
   
   const builder = new StringBuilder(".");
   console.log(builder.getValue()); // "."
